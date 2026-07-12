dea58d251e1058a55bfab2f8be70d13fe2cb5c8a
Felix-W2003 <rey_relia@qq.com>
2026-07-12 08:59:25 +0800

    08点59分

--- day06/并发编程-互斥锁使用.go
+++ day06/并发编程-互斥锁使用.go
@@ -41,3 +41,9 @@ func main() {
 
 //在使用RLock()和RUnlock()、Lock()和Unlock()等函数
 //它们必须成对出现
+/* Go语言的并发是通过goroutine完成的。goroutine是一种
+非常轻量级的实现，可在单个进程里执行成千上万个并发任务，
+它是go语言并发设计的核心。goroutine也可以被视作线程
+但他比线程更小，使用go关键字就可以创建goroutine将
+go关键字放到一个需调用
+*/
