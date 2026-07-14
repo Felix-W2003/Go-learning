d6733ecd27985fa2da62f1369887d153550ab453
Felix-W2003 <rey_relia@qq.com>
2026-07-14 15:15:55 +0800

    2026年7月14日15:15:54

--- gin-demo/ginstudy02/main.go
+++ gin-demo/ginstudy02/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type UserInfo struct {
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+type Article struct {
+	Title   string `json:"title" xml:"title"`
+	Content string `json:"content" xml:"content"`
+}
 
 func main() {
 	r := gin.Default()
@@ -28,5 +39,75 @@ func main() {
 			"title": "新闻",
 		})
 	})
+
+	r.GET("/article", func(ctx *gin.Context) {
+		id := ctx.DefaultQuery("id", "1")
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "新闻详情页面",
+			"id":  id,
+		})
+	})
+
+	r.GET("/user", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "default/user.html", gin.H{})
+	})
+
+	r.POST("/doAddUser", func(ctx *gin.Context) {
+		username := ctx.PostForm("username")
+		password := ctx.PostForm("password")
+		age := ctx.DefaultPostForm("age", "20")
+		ctx.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+			"age":      age,
+		})
+	})
+
+	r.GET("/getuser", func(ctx *gin.Context) {
+		user := &UserInfo{}
+
+		if err := ctx.ShouldBind(&user); err == nil {
+			fmt.Printf("%#v", user)
+			ctx.JSON(http.StatusOK, user)
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{
+				"err": err.Error(),
+			})
+		}
+	})
+	r.POST("/doAddUser2", func(ctx *gin.Context) {
+		user := &UserInfo{}
+
+		if err := ctx.ShouldBind(&user); err == nil {
+			fmt.Printf("%#v", user)
+			ctx.JSON(http.StatusOK, user)
+		} else {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
+	})
+
+	r.DELETE("/delete", func(ctx *gin.Context) {
+		//article := &Article{}
+		xmlSliceData, _ := ctx.GetRawData()
+		fmt.Println(xmlSliceData)
+		article := &Article{}
+		if err := xml.Unmarshal(xmlSliceData, &article); err == nil {
+			fmt.Printf("%#v", article)
+			ctx.JSON(http.StatusOK, article)
+		} else {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
+
+	})
+
+	r.GET("/list/:cid", func(ctx *gin.Context) {
+		cid := ctx.Param("cid")
+		ctx.String(200, "%v", cid)
+	})
+
 	r.Run()
 }
